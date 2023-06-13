a06eddc2405f3c69ac0e1eb955819fc3ee69b3fc
cor <cor@pruijs.dev>
2023-06-13 17:34:44 +0200

    feat: check if working directory is clean

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # update-deps-pr
 A simple tool to run `nix flake update` and open a PR
+
+## How to use
+
+1. Go to the repo for which you want to update the flake's dependencies.
+2. Checkout `main` and ensure that your working directory is clean.
+3. `nix run "github:cor/update-deps-pr"`
--- flake.nix
+++ flake.nix
@@ -15,19 +15,24 @@
             runtimeInputs = with pkgs; [ gh coreutils git nix ];
 
             text = ''
-              DATE=$(date --iso-8601)
-              echo "$DATE"
-              BRANCH="update-deps-$DATE"
-              git checkout -b "$BRANCH"
-              GIT_LFS_SKIP_SMUDGE=1 nix flake update
-              git add -A
-              git commit -m "chore: update deps $DATE" -m "Generated using [update-deps-pr](https://github.com/cor/update-deps-pr)"
-              git push -u origin "$BRANCH"
+              if [ -z "$(git status --porcelain)" ]; then 
+                DATE=$(date --iso-8601)
+                echo "$DATE"
+                BRANCH="update-deps-$DATE"
+                git checkout -b "$BRANCH"
+                GIT_LFS_SKIP_SMUDGE=1 nix flake update
+                git add -A
+                git commit -m "chore: update deps $DATE" -m "(generated using https://github.com/cor/update-deps-pr)"
+                git push -u origin "$BRANCH"
 
-              echo -e "Update deps by running \`nix flake update\`\n\n*(This PR is generated using [update-deps-pr](https://github.com/cor/update-deps-pr))*" > git-commit-msg
-              export MSG=$(cat git-commit-msg)
-              gh pr create -t "Update deps ($DATE)" -b "$MSG"
-              rm git-commit-msg
+                echo -e "Update deps by running \`nix flake update\`\n\n*(This PR is generated using [update-deps-pr](https://github.com/cor/update-deps-pr))*" > git-commit-msg
+                MSG=$(cat git-commit-msg)
+                export MSG
+                gh pr create -t "Update deps ($DATE)" -b "$MSG"
+                rm git-commit-msg
+              else 
+                echo "Please make sure that your working directory is clean and try again."
+              fi
             '';
           };
         };
